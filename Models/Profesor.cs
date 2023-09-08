ac4631cb18923550a688d06662e99bf721f0c306
matiasmacarioo <matiasmacarioo@gmail.com>
2023-09-07 21:06:57 -0300

    fecha

--- Models/Profesor.cs
+++ Models/Profesor.cs
@@ -14,6 +14,9 @@ namespace Proyecto.Models
         public string? Direccion { get; set; }
         public DateTime FechaNacimiento { get; set; }
 
+        [NotMapped]
+        public string FechaString { get => FechaNacimiento.ToString("yyyy-MM-dd");}
+    
         [ForeignKey("Carrera")]
         public int CarreraID { get; set; }
         public Carrera? Carrera { get; set; }
